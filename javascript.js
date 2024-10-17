ae3f3b09cd1748bb936c4660aea3db1ab2b590e5
Oliver-Rose-University <oliver.rose@student.norwichuni.ac.uk>
2024-10-17 14:34:17 +0100

    working song timer

--- javascript.js
+++ javascript.js
@@ -6,6 +6,9 @@ const playPauseButton = document.getElementById("play-button");
 const progressSlider = document.getElementById("progress-slider");
 
 const volumeSlider = document.getElementById("volume-slider");
+
+const songProgress = document.getElementById("song-progress");
+const songLength = document.getElementById("song-length");
 //audioPlayer.src is first song in playlist
 audioPlayer.src = "assets/duck1.mp3";
 
@@ -27,22 +30,33 @@ function onPlayPauseClick(){
 function onLoadedMetadata(){
     console.log(audioPlayer.duration);
     progressSlider.max = audioPlayer.duration;
+    songLength.innerHTML = secondsToMMSS(audioPlayer.duration);
 }
 
 function onTimeUpdate(){
     progressSlider.value = audioPlayer.currentTime;
+    songProgress.innerHTML = secondsToMMSS(audioPlayer.currentTime);
 }
 
 function onEnd(){
-    progressSlider.value = 0
-    playPauseButton.innerHTML = "Play"
+    progressSlider.value = 0;
+    playPauseButton.innerHTML = "Play";
     playing = false;
+    songProgress.innerHTML = "00:00";
 }
 
 function onVolumeSliderChange(){
-    audioPlayer.volume = parseInt(volumeSlider.value) * 0.01;
+    audioPlayer.volume = volumeSlider.value * 0.01;
 }
 
+function secondsToMMSS(seconds){
+    const intergerSeconds = parseInt(seconds);
+    let MM = parseInt(intergerSeconds / 60);
+    if (MM < 10) MM = "0" + MM;
+    let SS = intergerSeconds % 60;
+    if (SS < 10) SS = "0" + SS;
+    return MM + ":" + SS;
+}
 
 playPauseButton.onclick = onPlayPauseClick;
 audioPlayer.onloadedmetadata = onLoadedMetadata;
