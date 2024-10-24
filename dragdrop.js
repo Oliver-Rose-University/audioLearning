0e78d0a7dd0e0a96e4e52a2e851cec5e35cb6eb1
Oliver-Rose-University <oliver.rose@student.norwichuni.ac.uk>
2024-10-24 10:47:02 +0100

    comments..

--- dragdrop.js
+++ dragdrop.js
@@ -1,20 +1,22 @@
+// defines the audiplayer, which is the element we will be moving
 const audioplayer = document.getElementById("div1");
+// defines the dropzone, which is the entire page
 const dropZone = document.getElementById("body");
-
+// runs when the picked-up object is dropped, adjusting its position
 function onDrop(event) {
     audioplayer.style.left = event.clientX - offsetX + "px";
     audioplayer.style.top = event.clientY - offsetY + "px";
     console.log("Element has been dropped");
 }
-
+// is used for bugtesting purposes
 function onDragOver(event) {
     event.preventDefault();
     console.log("Something is being dragged over me!");
 }
-
+// defines the variables that will be used to change the positiom
 let offsetX = 0;
 let offsetY = 0;
-
+// runs when you begin to move the audio player
 function onDragStart(event) {
     const style = window.getComputedStyle(audioplayer, null);
 
@@ -23,6 +25,7 @@ function onDragStart(event) {
     console.log("I'm being Dragged");
 }
 
+// more definitions of events to functions
 dropZone.ondrop = onDrop;
 dropZone.ondragover = onDragOver;
 
